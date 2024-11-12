837cb8e21ccb55f852043b01ccb3761c93e0f4ab
okang1014 <okang1014@gmail.com>
2024-11-12 15:04:42 +0900

    feat: Ch2 리액트 시작하기 - Vite 개발 환경 구축

---
+++ workspace/ch02-start/vite/src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "@pages/todo/Todo";
+
+function App() {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+}
+
+export default App;
---
+++ workspace/ch02-start/vite/src/components/Title.jsx
@@ -0,0 +1,6 @@
+function Title() {
+  // 컴포넌트의 리턴값은 UI 리턴
+  return <h1>Vite 프로젝트</h1>;
+}
+
+export default Title;

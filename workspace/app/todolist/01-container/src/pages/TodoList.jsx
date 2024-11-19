61ab551697dfd2ddba34b184431c71657226c54c
okang1014 <okang1014@gmail.com>
2024-11-19 11:07:15 +0900

    feat: Ch2 리액트 시작하기 - TodoList 종합 prop-type 적용
    feat: Ch2 리액트 시작하기 - TodoList 종합 prop-type 적용

--- workspace/app/todolist/01-container/src/pages/TodoList.jsx
+++ workspace/app/todolist/01-container/src/pages/TodoList.jsx
@@ -5,7 +5,7 @@ function TodoList({ itemList, toggleDone, deleteItem }) {
     <TodoItem
       key={item._id}
       item={item}
-      toggleDone={toggleDone}
+      toggleDone={toggleDone} // 하위 컴포넌트의 PropType 과 다른 이름으로 props 를 전달하는 경우, 오류 발생, 콘솔에 오류 메시지 출력
       deleteItem={deleteItem}
     />
   ));

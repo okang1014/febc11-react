cddbdbfd0c8deb5e489595e2793495925335b4b5
okang1014 <okang1014@gmail.com>
2024-11-20 15:36:28 +0900

    feat: Ch3 리액트 훅 - useRef 훅 기능 2

--- workspace/ch03-hooks/04-useRef/src/components/Counter.jsx
+++ workspace/ch03-hooks/04-useRef/src/components/Counter.jsx
@@ -19,6 +19,9 @@ function Counter({ children = "0" }) {
   // 별도의 setter 함수 없음, 조작하기 위해 step.current 에 값을 지정하기만 하면 됨
   // 값이 변경되어도 렌더링이 필요가 없지만 언젠가는 값을 사용해야하는 경우 useRef 사용
 
+  const stepElem = useRef(null);
+  // useRef 를 사용하고, 선택하고자 하는 태그에 ref 속성을 추가하면 해당 DOM Node 를 선택할 수 있음
+
   const handleDown = () => {
     // setCount(count - step);
     countDispatch({ type: "DOWN", value: step.current });
@@ -32,6 +35,15 @@ function Counter({ children = "0" }) {
   const handleReset = (event) => {
     // setCount(initCount);
     countDispatch({ type: "RESET", value: initCount });
+
+    // JS 에서는 DOM API 를 사용해서 DOM Node 선택
+    // const stepElem = document.querySelector("#step");
+    // stepElem.focus();
+
+    console.log(stepElem); // {current: null}
+    // 하지만 React 에서는 DOM API 를 사용하게 된다면, virtual DOM 과 브라우저 DOM 이 어긋나게 됨 -> 예상치 못한 버그, 오류 발생 가능
+    stepElem.current.focus();
+    // ref 를 DOM Node 속성에 추가하는 경우 current 객체에는 DOM Node 객체가 추가됨
   };
 
   return (
@@ -44,6 +56,7 @@ function Counter({ children = "0" }) {
         style={{ width: "40px" }}
         defaultValue={step.current}
         // 브라우저에서 사용하는 input 요소는 그대로, 그리고 값이 변경되는 경우 변경된 값을 적용하는 코드만 지정
+        ref={stepElem}
         onChange={(e) => (step.current = Number(e.target.value))}
       />
       <Button type="button" color="red" onClick={handleDown}>

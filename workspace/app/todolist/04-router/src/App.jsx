9b8688fda89b3ddfba9fc6b6aeb1a0a983b63008
okang1014 <okang1014@gmail.com>
2024-11-25 13:10:41 +0900

    feat: Ch5 리액트 라우터 - TodoList 라우터 적용(React.Suspense 컴포넌트)

--- workspace/app/todolist/04-router/src/App.jsx
+++ workspace/app/todolist/04-router/src/App.jsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 // import router from "./routes";
 import router from "./routes-lazy";
+import { Suspense } from "react";
+import { PulseLoader } from "react-spinners";
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
+      {/* <Suspense fallback={<PulseLoader />}> */}
       <RouterProvider router={router} future={{ v7_startTransition: true }} />
-    </>
+    </Suspense>
   );
 }
 

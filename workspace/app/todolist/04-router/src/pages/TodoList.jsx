6cba722975ca5f923b4a4c93bf2db1d148cc27c9
okang1014 <okang1014@gmail.com>
2024-11-25 10:25:46 +0900

    feat: Ch5 리액트 라우터 - TodoList 라우터 적용 과제 풀이(feat. 상대경로와 절대경로의 중요성)

--- workspace/app/todolist/04-router/src/pages/TodoList.jsx
+++ workspace/app/todolist/04-router/src/pages/TodoList.jsx
@@ -2,38 +2,41 @@ import { Link } from "react-router-dom";
 
 function TodoList() {
   return (
-    <>
-      <div id="main">
-        <h2>할일 목록</h2>
-        <div className="todo">
-          <Link to="./todoadd">추가</Link>
-          <br />
-          <form className="search">
-            <input type="text" autoFocus />
-            <button type="submit">검색</button>
-          </form>
-          <ul className="todolist">
-            <li>
-              <span>1</span>
-              <Link to="./tododetail">잠자기</Link>
-              <Link to="./list">삭제</Link>
-            </li>
-            <li>
-              <span>2</span>
-              <Link to="./tododetail">자바스크립트 복습</Link>
-              <Link to="./list">삭제</Link>
-            </li>
-            <li>
-              <span>3</span>
-              <Link to="./tododetail">
-                <s>리액트 과제 하기</s>
-              </Link>
-              <Link to="./list">삭제</Link>
-            </li>
-          </ul>
-        </div>
+    <div id="main">
+      <h2>할일 목록</h2>
+      <div className="todo">
+        {/* /list 를 /add 로 교체, 현재 상황에서는 모두 동일한 방식으로 교체 */}
+        {/* 절대 경로는 서비스 규모가 큰 경우, 전체 절대 경로를 추가해야함 */}
+        <Link to="/add">추가</Link>
+        {/* Link to 로 상대 경로를 지정하는 경우, 전체 url 이후에 link 가 추가됨, 그렇기 때문에 되지 않았던 것이었다 */}
+        {/* 상대 경로 또는 절대 경로를 사용해야하는 경우가 있지만, 일단 기본적으로 절대 경로로 지정하는 것이 좋다 */}
+        <br />
+        <form className="search">
+          <input type="text" autoFocus />
+          <button type="submit">검색</button>
+        </form>
+        <ul className="todolist">
+          <li>
+            <span>1</span>
+            <Link to="/list/1">잠자기</Link>
+            {/* 모든 a 태그의 url 이 동일한 경우, 어떤 항목의 상세인지 확인하기 어려움, 따라서 각 항목에 대한 id 가 필요 */}
+            <Link to="/list">삭제</Link>
+          </li>
+          <li>
+            <span>2</span>
+            <Link to="/list/2">자바스크립트 복습</Link>
+            <Link to="/list">삭제</Link>
+          </li>
+          <li>
+            <span>3</span>
+            <Link to="/list/3">
+              <s>리액트 과제 하기</s>
+            </Link>
+            <Link to="/list">삭제</Link>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
 
--- workspace/app/todolist/04-router/src/routes.jsx
+++ workspace/app/todolist/04-router/src/routes.jsx
@@ -14,14 +14,19 @@ const router = createBrowserRouter(
       path: "/",
       element: <Layout />,
       children: [
+        { index: true, element: <Navigate to="/home" /> }, // / 까지만 입력되고 이후 주소값이 없는 경우, home 으로 이동
         { path: "home", element: <Home /> },
         { path: "about", element: <About /> },
         { path: "list", element: <TodoList /> },
-        { path: "list/list", element: <Navigate to="/list" /> },
-        { path: "list/todoadd", element: <TodoAdd /> },
-        { path: "list/tododetail", element: <TodoDetail /> },
-        { path: "list/todoedit", element: <TodoEdit /> },
-        { path: "list/todoerror", element: <ErrorPage /> },
+        // REST API - 경로는 주소만, 그리고 URL 은 브라우저가 봤을 때 동일한 컨텐츠가 나오도록 만들어주어야 함
+        // 라우터 설계하였을 때 계속 depth 가 깊어지는 상황이 생긴다(내가 그랬다)
+        // 이는 각 페이지에서 경로를 절대경로로 지정함으로써 해결할 수 있음
+        // React Router 의 Link to 와 a href 의 차이
+        { path: "add", element: <TodoAdd /> },
+        { path: "edit", element: <TodoEdit /> },
+        // 상세 페이지의 url 에서 각 게시물의 id 지정, path 에 :_id 를 지어줌으로써 동적으로 url 이 지정되도록 함(세그먼트)
+        { path: "list/:_id", element: <TodoDetail /> }, // TodoDetail 에서 useParams() 리액트 라우터 hook 을 사용하여 id 값을 동적으로 지정할 수 있음
+        // : 이후에는 변수 이름이 됨
       ],
     },
   ],

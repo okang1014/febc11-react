1d899a64c3f0cc47f8fde062ed82eca73771eeb3
okang1014 <okang1014@gmail.com>
2024-12-09 11:26:34 +0900

    feat: Skeleton Project Lion-board 게시글 상세조회 기능 추가
    feat: Skeleton Project Lion-board 게시글 상세조회 기능 추가

--- workspace/ch11-skeleton/lion-board/src/pages/board/ListItem.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/ListItem.jsx
@@ -19,7 +19,8 @@ export default function ListItem({ item }) {
     <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
       <td className="p-2 text-center">{item._id}</td>
       <td className="p-2 truncate indent-4">
-        <Link to="/info/1" className="cursor-pointer">
+        <Link to={`${item._id}`} className="cursor-pointer">
+          {/* 현재 주소 뒤에 id 를 추가 */}
           {item.title}
         </Link>
       </td>

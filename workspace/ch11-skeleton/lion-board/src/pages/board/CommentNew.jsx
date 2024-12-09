46d1b87f4fcdd83377670d7b2953a0ad212b2e94
okang1014 <okang1014@gmail.com>
2024-12-09 16:46:13 +0900

    feat: Skeleton Project Lion-board 댓글 작성 기능 추가

--- workspace/ch11-skeleton/lion-board/src/pages/board/CommentNew.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/CommentNew.jsx
@@ -1,18 +1,56 @@
+import InputError from "@components/InputError";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+
 export default function CommentNew() {
+  const axios = useAxiosInstance();
+
+  const { type, _id } = useParams();
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const queryClient = useQueryClient();
+
+  const addComment = useMutation({
+    mutationFn: (formData) => axios.post(`/posts/${_id}/replies`, formData),
+    onSuccess: () => {
+      alert("댓글이 등록되었습니다.");
+      queryClient.invalidateQueries(["posts", type, _id, "replies"]);
+      navigate(`/${type}/${_id}`);
+      // react-hook-form 에서 input field 초기화하는 함수 reset()
+      reset();
+    },
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
   return (
     <div className="p-4 border border-gray-200 rounded-lg">
       <h4 className="mb-4">새로운 댓글을 추가하세요.</h4>
-      <form action="#">
+      <form onSubmit={handleSubmit(addComment.mutate)}>
         <div className="mb-4">
           <textarea
             rows="3"
             cols="40"
             className="block p-2 w-full text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             placeholder="내용을 입력하세요."
-            name="comment"
+            {...register("content", { required: "내용은 필수입니다." })}
           ></textarea>
-
-          <p className="ml-2 mt-1 text-sm text-red-500">내용은 필수입니다.</p>
+          <InputError target={errors.title} />
         </div>
         <button
           type="submit"

bdf19e86c9ec326a98fda3c4f8ad0798cee2bcbc
okang1014 <okang1014@gmail.com>
2024-12-09 17:31:27 +0900

    feat: Skeleton Project Lion-board 댓글 목록 조회 기능 수정(댓글이 없는 케이스 추가)
    feat: Skeleton Project Lion-board 댓글 목록 조회 기능 수정(댓글이 없는 케이스 추가)

--- workspace/ch11-skeleton/lion-board/src/pages/board/CommentList.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/CommentList.jsx
@@ -10,8 +10,19 @@ CommentList.propTypes = {
 };
 
 export default function CommentList({ replies }) {
+  // if (!replies) {
+  //   return <div>댓글 로딩 중...</div>;
+  // }
   if (!replies) {
-    return <div>댓글 로딩 중...</div>;
+    return (
+      <section className="mb-8">
+        <h4 className="mt-8 mb-4 ml-2">댓글 0개</h4>
+
+        <div>표시할 댓글이 없습니다.</div>
+
+        <CommentNew />
+      </section>
+    );
   }
 
   const comments = replies.map((item) => (

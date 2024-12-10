2ee4e2af216415284aae9dc69fba82b710b9db42
okang1014 <okang1014@gmail.com>
2024-12-10 16:02:00 +0900

    feat: Skeleton Project Lion-board 로그인 상태 유지 관련 기능 추가

--- workspace/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
+++ workspace/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
@@ -1,6 +1,8 @@
+import useUserStore from "@zustand/userStore";
 import axios from "axios";
 
 function useAxiosInstance() {
+  const { user } = useUserStore();
   const instance = axios.create({
     baseURL: 'https://11.fesp.shop',
     timeout: 1000 * 15,
@@ -13,7 +15,11 @@ function useAxiosInstance() {
 
   // 요청 인터셉터 추가하기
   instance.interceptors.request.use((config) => {
-    config.headers['Authorization'] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjc4LCJ0eXBlIjoidXNlciIsIm5hbWUiOiLqsJXsp4Dtm4giLCJlbWFpbCI6ImppaG9vbkB0ZXN0LmNvbSIsImltYWdlIjp7Im9yaWdpbmFsbmFtZSI6ImVmYzU4MWMwYzA4MmQxN2IuanBnIiwibmFtZSI6IkUwcWZhQk4zRC5qcGciLCJwYXRoIjoiL2ZpbGVzLzAwLWJydW5jaC9FMHFmYUJOM0QuanBnIn0sImxvZ2luVHlwZSI6ImVtYWlsIiwiaWF0IjoxNzMzODA1ODkyLCJleHAiOjE3MzM4OTIyOTIsImlzcyI6IkZFU1AifQ.bA1QWEIqnZL3I27cdvy91lyEn9ukEZTFWVmIl8UvAeM`
+    if (user) {
+      // 회원가입이 된 상태
+      config.headers['Authorization'] = `Bearer ${user.accessToken}`;
+    }
+    // config.headers['Authorization'] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjc4LCJ0eXBlIjoidXNlciIsIm5hbWUiOiLqsJXsp4Dtm4giLCJlbWFpbCI6ImppaG9vbkB0ZXN0LmNvbSIsImltYWdlIjp7Im9yaWdpbmFsbmFtZSI6ImVmYzU4MWMwYzA4MmQxN2IuanBnIiwibmFtZSI6IkUwcWZhQk4zRC5qcGciLCJwYXRoIjoiL2ZpbGVzLzAwLWJydW5jaC9FMHFmYUJOM0QuanBnIn0sImxvZ2luVHlwZSI6ImVtYWlsIiwiaWF0IjoxNzMzODA1ODkyLCJleHAiOjE3MzM4OTIyOTIsImlzcyI6IkZFU1AifQ.bA1QWEIqnZL3I27cdvy91lyEn9ukEZTFWVmIl8UvAeM`
     config.params = {
       delay: 500,
       ...config.params,

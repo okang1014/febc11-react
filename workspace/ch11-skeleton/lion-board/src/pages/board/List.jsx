a0e757d7d5f10d3a75c94e20f8f3c6bbb8526df0
okang1014 <okang1014@gmail.com>
2024-12-13 15:25:51 +0900

    feat: Skeleton Project Lion-board list type 에 따라 동적으로 페이지 제목 및 meta 태그 변경

--- workspace/ch11-skeleton/lion-board/src/pages/board/List.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/List.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import useUserStore from "@zustand/userStore";
+import { Helmet } from "react-helmet-async";
 
 export default function List() {
   const axios = useAxiosInstance();
@@ -19,6 +20,29 @@ export default function List() {
     staleTime: 1000 * 10, // 캐시 시간
   });
 
+  const printListType = (type) => {
+    let listTitle = "";
+    switch (type) {
+      case "info":
+        listTitle = "정보 공유";
+        break;
+      case "free":
+        listTitle = "자유 게시판";
+        break;
+      case "qna":
+        listTitle = "Q&A's";
+        break;
+      case "brunch":
+        listTitle = "브런치 스토리";
+        break;
+      default:
+        listTitle = "멋사컴에 오신 걸 환영합니다.";
+        break;
+    }
+
+    return listTitle;
+  };
+
   console.log(data);
 
   if (!data) {
@@ -28,81 +52,92 @@ export default function List() {
   const list = data.item.map((item) => <ListItem key={item._id} item={item} />);
 
   return (
-    <main className="min-w-80 p-10">
-      <div className="text-center py-4">
-        <h2 className="pb-4 text-2xl font-bold text-gray-700 dark:text-gray-200">
-          정보 공유
-        </h2>
-      </div>
-      <div className="flex justify-end mr-4">
-        <form action="#">
-          <input
-            className="dark:bg-gray-600 bg-gray-100 p-1 rounded"
-            type="text"
-            name="keyword"
-          />
-          <button
-            type="submit"
-            className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-          >
-            검색
-          </button>
-        </form>
+    <>
+      <Helmet>
+        <title> {printListType(type)}- 멋사컴</title>
 
-        {/* 조건부 렌더링, 로그인 사용자만 글 작성 가능 
+        <meta property="og:title" content={`${printListType(type)} - 멋사컴`} />
+        <meta
+          property="og:description"
+          content={`멋사컴 ${printListType(type)}입니다.`}
+        />
+      </Helmet>
+      <main className="min-w-80 p-10">
+        <div className="text-center py-4">
+          <h2 className="pb-4 text-2xl font-bold text-gray-700 dark:text-gray-200">
+            {printListType(type)}
+          </h2>
+        </div>
+        <div className="flex justify-end mr-4">
+          <form action="#">
+            <input
+              className="dark:bg-gray-600 bg-gray-100 p-1 rounded"
+              type="text"
+              name="keyword"
+            />
+            <button
+              type="submit"
+              className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+            >
+              검색
+            </button>
+          </form>
+
+          {/* 조건부 렌더링, 로그인 사용자만 글 작성 가능 
         완벽하게 로그인 하지 않은 사용자 글쓰기 접속을 막은 것은 아님
         직접 링크를 접속하게 된다면 글쓰기 페이지 이동 가능 */}
-        {user && (
-          <Link
-            to="new"
-            // 현재 url 에 new 로 이동
-            className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-          >
-            글작성
-          </Link>
-        )}
-      </div>
-      <section className="pt-10">
-        <table className="border-collapse w-full table-fixed">
-          <colgroup>
-            <col className="w-[10%] sm:w-[10%]" />
-            <col className="w-[60%] sm:w-[30%]" />
-            <col className="w-[30%] sm:w-[15%]" />
-            <col className="w-0 sm:w-[10%]" />
-            <col className="w-0 sm:w-[10%]" />
-            <col className="w-0 sm:w-[25%]" />
-          </colgroup>
-          <thead>
-            <tr className="border-b border-solid border-gray-600">
-              <th className="p-2 whitespace-nowrap font-semibold">번호</th>
-              <th className="p-2 whitespace-nowrap font-semibold">제목</th>
-              <th className="p-2 whitespace-nowrap font-semibold">글쓴이</th>
-              <th className="p-2 whitespace-nowrap font-semibold hidden sm:table-cell">
-                조회수
-              </th>
-              <th className="p-2 whitespace-nowrap font-semibold hidden sm:table-cell">
-                댓글수
-              </th>
-              <th className="p-2 whitespace-nowrap font-semibold hidden sm:table-cell">
-                작성일
-              </th>
-            </tr>
-          </thead>
-          <tbody>{list}</tbody>
-        </table>
-        <hr />
-
-        <div>
-          <ul className="flex justify-center gap-3 m-4">
-            <li className="font-bold text-blue-700">
-              <Link to="/info?page=1">1</Link>
-            </li>
-            <li>
-              <Link to="/info?page=2">2</Link>
-            </li>
-          </ul>
+          {user && (
+            <Link
+              to="new"
+              // 현재 url 에 new 로 이동
+              className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+            >
+              글작성
+            </Link>
+          )}
         </div>
-      </section>
-    </main>
+        <section className="pt-10">
+          <table className="border-collapse w-full table-fixed">
+            <colgroup>
+              <col className="w-[10%] sm:w-[10%]" />
+              <col className="w-[60%] sm:w-[30%]" />
+              <col className="w-[30%] sm:w-[15%]" />
+              <col className="w-0 sm:w-[10%]" />
+              <col className="w-0 sm:w-[10%]" />
+              <col className="w-0 sm:w-[25%]" />
+            </colgroup>
+            <thead>
+              <tr className="border-b border-solid border-gray-600">
+                <th className="p-2 whitespace-nowrap font-semibold">번호</th>
+                <th className="p-2 whitespace-nowrap font-semibold">제목</th>
+                <th className="p-2 whitespace-nowrap font-semibold">글쓴이</th>
+                <th className="p-2 whitespace-nowrap font-semibold hidden sm:table-cell">
+                  조회수
+                </th>
+                <th className="p-2 whitespace-nowrap font-semibold hidden sm:table-cell">
+                  댓글수
+                </th>
+                <th className="p-2 whitespace-nowrap font-semibold hidden sm:table-cell">
+                  작성일
+                </th>
+              </tr>
+            </thead>
+            <tbody>{list}</tbody>
+          </table>
+          <hr />
+
+          <div>
+            <ul className="flex justify-center gap-3 m-4">
+              <li className="font-bold text-blue-700">
+                <Link to="/info?page=1">1</Link>
+              </li>
+              <li>
+                <Link to="/info?page=2">2</Link>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }

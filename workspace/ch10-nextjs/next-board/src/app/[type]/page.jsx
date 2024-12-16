2fe9a88521f1d031203a9d4aadb376ef60f2ef46
okang1014 <okang1014@gmail.com>
2024-12-16 13:03:49 +0900

    feat: Ch10 Next.js 게시글 리스트 출력

--- workspace/ch10-nextjs/next-board/src/app/[type]/page.jsx
+++ workspace/ch10-nextjs/next-board/src/app/[type]/page.jsx
@@ -1,18 +1,40 @@
 // 동적 라우팅 설정 시, 해당 폴더를 [동적 라우팅 명(변수명)] 으로 지정하면 Next.js 에 의해 자동으로 라우팅 설정
 // 자료 구조가 결국 라우팅 경로가 됨, 따라서 더더욱 자료 구조 관리가 중요
 
+import ListItem from "@/app/[type]/ListItem";
 import Link from "next/link";
+
+// 게시물 목록 조회 후 반환
+async function fetchPosts(type) {
+  // api 서버 주소에 type 값 전달
+  const url = `https://11.fesp.shop/posts?type=${type}`;
+
+  // Next.js 에서는 API 서버 호출은 fetch API 에 캐싱 기능이 추가됨
+  const res = await fetch(url, {
+    headers: { "client-id": "00-board" },
+  });
+  return await res.json(); // 서버 응답으로 전달받은 데이터를 parsing
+}
 
 // react-router-dom 에서는 useParams 로 해당 url 중 list 의 type 을 획득하여, 해당 리스트를 동적으로 페이지에 출력
 // next.js 에서는 params 라는 prop 으로 자동으로 전달되어 사용 가능
 // React 에서는 컴포넌트를 async 함수로 사용할 수 없음
 // 하지만 Next.js 에서는(서버 컴포넌트 한정) async 함수로 사용할 수 있음
 export default async function Page({ params }) {
+  // react 에서는 const {type} = useParams(); 로 해당 게시판의 type 을 url 로부터 전달받음
   // const { type } = params; // params 객체 내의 type 을 구조 분해 할당, next.js 14 까지는 props 를 바로 사용할 수 있었음
   // Next.js 15 에서는 params 가 Promise 로 전달됨, 따라서 promise 가 fulfilled 되는 것을 기다려야 함.
+
+  // Next.js 에서는 컴포넌트를 서버 측에서 렌더링을 완료하여 화면에 반환, 따라서 React 컴폰넌트와 달리, 데이터가 없는 경우의 예외 처리가 불필요
+
   const { type } = await params;
 
-  // react 에서는 const {type} = useParams(); 로 해당 게시판의 type 을 url 로부터 전달받음
+  const data = await fetchPosts(type);
+
+  console.log(data.item.length, "건 조회됨.");
+
+  const list = data.item.map((item) => <ListItem key={item._id} item={item} />);
+  // data 내부의 item 의 개수만큼 반복해서 ListItem 컴포넌트 생성, ListItem 컴포넌트에 key 를 item._id 로 지정, item 에 props 로 전달
 
   return (
     <main className="min-w-80 p-10">
@@ -69,36 +91,7 @@ export default async function Page({ params }) {
               </th>
             </tr>
           </thead>
-          <tbody>
-            <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-              <td className="p-2 text-center">2</td>
-              <td className="p-2 truncate indent-4">
-                <Link href="/info/2" className="cursor-pointer">
-                  안녕하세요.
-                </Link>
-              </td>
-              <td className="p-2 text-center truncate">용쌤</td>
-              <td className="p-2 text-center hidden sm:table-cell">29</td>
-              <td className="p-2 text-center hidden sm:table-cell">2</td>
-              <td className="p-2 truncate text-center hidden sm:table-cell">
-                2024.07.05 13:39:23
-              </td>
-            </tr>
-            <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-              <td className="p-2 text-center">1</td>
-              <td className="p-2 truncate indent-4">
-                <Link href="/info/1" className="cursor-pointer">
-                  좋은 소식이 있습니다.
-                </Link>
-              </td>
-              <td className="p-2 text-center truncate">제이지</td>
-              <td className="p-2 text-center hidden sm:table-cell">22</td>
-              <td className="p-2 text-center hidden sm:table-cell">5</td>
-              <td className="p-2 truncate text-center hidden sm:table-cell">
-                2024.07.03 17:59:13
-              </td>
-            </tr>
-          </tbody>
+          <tbody>{list}</tbody>
         </table>
         <hr />
 
--- workspace/ch10-nextjs/next-board/src/app/error.jsx
+++ workspace/ch10-nextjs/next-board/src/app/error.jsx
@@ -1,13 +1,15 @@
 "use client"; // client 컴포넌트로 사용될 컴포넌트
 
-export default function Error() {
+export default function Error({ error, reset }) {
+  console.error(error);
   return (
     <div className="py-20 bg-red-100 border border-red-400 text-red-700 p-4 rounded-lg flex flex-col items-center space-y-2">
       <h2 className="text-xl font-semibold mb-2 text-center">
         🚧 앗, 무언가 잘못됐네요!
       </h2>
       <h3 className="text-md font-semibold mb-2 text-center">
-        일시적인 오류가 발생했습니다.
+        일시적인 오류가 발생했습니다. <br />
+        {error.message}
       </h3>
       <p className="pt-12 text-center">
         이 오류는 더 나은 서비스를 위한 첫걸음이에요. 조금만 기다려 주세요!
